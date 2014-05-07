89013ef7becf981c9693e0fdc8f577c1d05e69b6
Henrik Almér <henrik.almer@gmail.com>
2014-05-07 13:41:12 +0200

    Test LoadImage on debug data

--- Tests.m
+++ Tests.m
@@ -4,6 +4,8 @@ classdef Tests < matlab.unittest.TestCase
     end
     
     methods (Test)
+        
+        %% Test LoadImage
         function testLoadImageIsNormalized(testCase)
             % Verifies that the returned data is normalized
             addpath('Pics/TrainingImages/FACES/');
@@ -18,6 +20,17 @@ classdef Tests < matlab.unittest.TestCase
             [im, ii_im] = LoadImage('face00001.bmp');
             testCase.verifyEqual(ii_im(2, 2), sum(sum(im(1:2, 1:2))))
         end
+        
+        function testLoadImage(testCase)
+            % Verify that output from face00001.bmp matched debug data
+            addpath('Pics/TrainingImages/FACES/');
+            [im, ii_im] = LoadImage('face00001.bmp');
+            dinfo1 = load('DebugInfo/debuginfo1.mat');
+            eps = 1e-6;
+            testCase.verifyEqual(im(:), dinfo1.im(:), 'AbsTol', eps);
+            testCase.verifyEqual(ii_im(:), dinfo1.ii_im(:), 'AbsTol', eps);
+        end
+        
     end
     
 end
