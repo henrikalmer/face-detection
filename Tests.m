e7364b5025f6e5ce714b8bcada9463810a956908
Henrik Almér <henrik.almer@gmail.com>
2014-05-07 15:00:32 +0200

    Add setup and teardown methods

--- Tests.m
+++ Tests.m
@@ -1,13 +1,26 @@
 classdef Tests < matlab.unittest.TestCase
     
     properties
+        OriginalPath
+    end
+    
+    methods (TestMethodSetup)
+        function addImagesToPath(testCase)
+            testCase.OriginalPath = path;
+            addpath('Pics/TrainingImages/FACES/');
+        end
+    end
+    
+    methods (TestMethodTeardown)
+        function restorePath(testCase)
+            path(testCase.OriginalPath);
+        end
     end
     
     methods (Test)
         %% Test LoadImage
         function testLoadImageIsNormalized(testCase)
             % Verifies that the returned data is normalized
-            addpath('Pics/TrainingImages/FACES/');
             im = LoadImage('face00001.bmp');
             testCase.verifyEqual(int64(mean(im(:))), int64(0));
             testCase.verifyEqual(int64(std(im(:))), int64(1));
@@ -15,14 +28,12 @@ classdef Tests < matlab.unittest.TestCase
         
         function testLoadImageCumsum(testCase)
             % Verifies that the integral image contains correct values
-            addpath('Pics/TrainingImages/FACES/');
             [im, ii_im] = LoadImage('face00001.bmp');
             testCase.verifyEqual(ii_im(2, 2), sum(sum(im(1:2, 1:2))))
         end
         
         function testLoadImage(testCase)
             % Verify that output from face00001.bmp matched debug data
-            addpath('Pics/TrainingImages/FACES/');
             [im, ii_im] = LoadImage('face00001.bmp');
             dinfo1 = load('DebugInfo/debuginfo1.mat');
             tol = 1e-6;
@@ -33,7 +44,6 @@ classdef Tests < matlab.unittest.TestCase
         %% Test ComputeBoxSum
         function testComputeBoxSum(testCase)
             % Verify that ComputeBoxSum does a correct calculation
-            addpath('Pics/TrainingImages/FACES/');
             [im, ii_im] = LoadImage('face00001.bmp');
             tol = 1e-6;
             inputs = [[1 1 4 6];
@@ -47,7 +57,7 @@ classdef Tests < matlab.unittest.TestCase
                 expected = sum(sum(im(y:y+h-1, x:x+w-1)));
                 actual = ComputeBoxSum(ii_im, x, y, w, h);
                 testCase.verifyEqual(actual, expected, 'AbsTol', tol);
-            end 
+            end
         end
         
         %% Test Feature computation
