1ea8ad725984deb86a4c2d04c7198f55b144bb69
Henrik Almér <henrik.almer@gmail.com>
2014-05-07 14:49:00 +0200

    Add function and test for computing sum of pixel values

---
+++ ComputeBoxSum.m
@@ -0,0 +1,21 @@
+function A = ComputeBoxSum(ii_im, x, y, w, h)
+%COMPUTEBOXSUM Computes the sum of pixel values in a rectangular area
+%   Uses the integral image 'ii_im' to compute the sum of pixel values in
+%   the original image 'im' in the rectangular area given by c, y, w and h.
+
+
+% Pad with zeros to avoid index errors when x or y is 1
+ii_im = padarray(ii_im, [1 1]);
+x = x+1;
+y = y+1;
+
+% Calculate sum
+a1 = ii_im(y+h-1, x+w-1);
+a2 = ii_im(y+h-1, x-1);
+a3 = ii_im(y-1, x+w-1);
+a4 = ii_im(y-1, x-1);
+
+A = a1 - a2 - a3 + a4;
+
+end
+
--- Tests.m
+++ Tests.m
@@ -4,7 +4,6 @@ classdef Tests < matlab.unittest.TestCase
     end
     
     methods (Test)
-        
         %% Test LoadImage
         function testLoadImageIsNormalized(testCase)
             % Verifies that the returned data is normalized
@@ -26,11 +25,26 @@ classdef Tests < matlab.unittest.TestCase
             addpath('Pics/TrainingImages/FACES/');
             [im, ii_im] = LoadImage('face00001.bmp');
             dinfo1 = load('DebugInfo/debuginfo1.mat');
-            eps = 1e-6;
-            testCase.verifyEqual(im(:), dinfo1.im(:), 'AbsTol', eps);
-            testCase.verifyEqual(ii_im(:), dinfo1.ii_im(:), 'AbsTol', eps);
+            tol = 1e-6;
+            testCase.verifyEqual(im(:), dinfo1.im(:), 'AbsTol', tol);
+            testCase.verifyEqual(ii_im(:), dinfo1.ii_im(:), 'AbsTol', tol);
         end
         
+        %% Test ComputeBoxSum
+        function testComputeBoxSum(testCase)
+            % Verify that ComputeBoxSum does a correct calculation
+            addpath('Pics/TrainingImages/FACES/');
+            [im, ii_im] = LoadImage('face00001.bmp');
+            tol = 1e-6;
+            points = [[1 1]; [1 2]; [2 1]; [2 2]; [3 5]];
+            for i = 1:size(points, 1)
+                x = points(i,1); y = points(i,2);
+                w = 6; h = 6;
+                expected = sum(sum(im(y:y+h-1, x:x+w-1)));
+                actual = ComputeBoxSum(ii_im, x, y, w, h);
+                testCase.verifyEqual(actual, expected, 'AbsTol', tol);
+            end 
+        end
     end
     
 end
